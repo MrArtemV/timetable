305b0aa104bc67e315e7e93646177ca34f6b2fc4
MrArtemV <56505485+MrArtemV@users.noreply.github.com>
2021-04-26 19:48:20 +0300

    complete reset-password menu

--- reset.php
+++ reset.php
@@ -8,22 +8,30 @@
 		$extra = 'index.php';
 		header("Location: http://$host$uri/$extra");
 	}
-	else {
 ?>
-
-<main class="container">
-	<div class="row">
-		<div class="col-lg-12">
-			<div class="main round pad_b_10">
-				<div class="ml-3 mr-3">
-					<h1 class="text-center title">Смена пароля</h1>
+<main class='container'>
+	<div class='row'>
+		<div class='col-lg-12'>
+			<div class='main round pad_b_10'>
+				<div class='ml-3 mr-3'>
+					<?php
+						if ($_REQUEST['pass']) {
+							$name = $_SESSION['user'];
+							$newpass = md5($_REQUEST['pass']);
+							$query = "UPDATE `users` SET `password` = '$newpass' WHERE `name` = '$name'";
+							$cat = $pdo->prepare($query);
+							$cat->execute();
+							echo "<h1 class='text-center title'> Пароль успешно изменён</h1><p class='point_desc'>Теперь вы можете перейти <a href='http://{$_SERVER['HTTP_HOST']}'>на главную</a></p>";
+						}
+						else {
+							echo "<h1 class='text-center title'>Смена пароля</h1><form action='' class='form-inline text-center mr-3 ml-3' method='POST'><div class='container'><div class='row'><div class='col-lg'><p class='point_desc'>Новый пароль:</p></div><div class='col-lg'><input type='text' class='form-control' name='pass'></div><div class='col-lg'><button type='submit' class='btn btn-primary'>Отправить</button></div></div></div></form>";
+						}
+					?>
 				</div>
 			</div>
 		</div>
 	</div>
 </main>
-
 <?
-	}
 	include 'footer.php';
 ?>
